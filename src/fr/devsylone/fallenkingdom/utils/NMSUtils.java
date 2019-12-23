ab036086cd02aca4fa1f2af706493864544e971c
syldium <syldium@mailo.com>
2019-12-23 17:59:45 +0100

    Add 1.13+ compatibility to the scoreboard

--- src/fr/devsylone/fallenkingdom/utils/NMSUtils.java
+++ src/fr/devsylone/fallenkingdom/utils/NMSUtils.java
@@ -1,6 +1,7 @@
 package fr.devsylone.fallenkingdom.utils;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
@@ -56,6 +57,46 @@ public class NMSUtils
 		}
 
 		throw new ClassNotFoundException("Cannot found this class : " + givenName + "(Is it registered ?)");
+	}
+
+
+	public static final String OBC_PACKAGE = "org.bukkit.craftbukkit";
+	public static final String NMS_PACKAGE = "net.minecraft.server";
+	public static String nmsClassName(String className) {
+		return NMS_PACKAGE + '.' + version + '.' + className;
+	}
+
+	public static Class<?> nmsClass(String className) throws ClassNotFoundException {
+		return Class.forName(nmsClassName(className));
+	}
+
+	public static Optional<Class<?>> nmsOptionalClass(String className) {
+		return optionalClass(nmsClassName(className));
+	}
+
+	public static String obcClassName(String className) {
+		return OBC_PACKAGE + '.' + version + '.' + className;
+	}
+
+	public static Class<?> obcClass(String className) throws ClassNotFoundException {
+		return Class.forName(obcClassName(className));
+	}
+
+	public static Optional<Class<?>> obcOptionalClass(String className) {
+		return optionalClass(obcClassName(className));
+	}
+
+	public static Optional<Class<?>> optionalClass(String className) {
+		try {
+			return Optional.of(Class.forName(className));
+		} catch (ClassNotFoundException e) {
+			return Optional.empty();
+		}
+	}
+
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	public static Object enumValueOf(Class<?> enumClass, String enumName) {
+		return Enum.valueOf((Class<Enum>) enumClass, enumName.toUpperCase());
 	}
 
 	public static String getVersion()
