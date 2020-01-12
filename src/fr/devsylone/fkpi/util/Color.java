00fc79c19ddf0618c86ed65c23f02a66312898ab
Faymoon <mateo.mercier@laposte.net>
2020-01-12 18:36:31 +0100

    little fix

--- src/fr/devsylone/fkpi/util/Color.java
+++ src/fr/devsylone/fkpi/util/Color.java
@@ -1,39 +1,40 @@
 package fr.devsylone.fkpi.util;
 
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 
 public enum Color
 {
 	
 
-	BLEU("bleu", "bleue", "blue", ChatColor.BLUE, (byte) 11),
-	ROUGE("rouge", "rouge", "red", ChatColor.RED, (byte) 14),
-	VERT("vert", "verte", "green", ChatColor.GREEN, (byte) 5),
-	VIOLET("violet", "violette", "purple", ChatColor.DARK_PURPLE, (byte) 10),
-	ROSE("rose", "rose", "rose", ChatColor.LIGHT_PURPLE, (byte) 6),
-	JAUNE("jaune", "jaune", "yellow", ChatColor.YELLOW, (byte) 4),
-	BLANC("blanc", "blanche", "white", ChatColor.WHITE, (byte) 0),
-	NOIR("noir", "noire", "black", ChatColor.BLACK, (byte) 15),
-	ORANGE("orange", "orange", "orange", ChatColor.GOLD, (byte) 1),
-	GRIS("gris", "grise", "gray", ChatColor.GRAY, (byte) 8),
-	NO_COLOR("no color", "no color", "no color", ChatColor.WHITE, (byte) 0);
+	BLEU("bleu", "bleue", "blue", ChatColor.BLUE, DyeColor.BLUE),
+	ROUGE("rouge", "rouge", "red", ChatColor.RED, DyeColor.RED),
+	VERT("vert", "verte", "green", ChatColor.GREEN, DyeColor.GREEN),
+	VIOLET("violet", "violette", "purple", ChatColor.DARK_PURPLE, DyeColor.PURPLE),
+	ROSE("rose", "rose", "rose", ChatColor.LIGHT_PURPLE, DyeColor.MAGENTA),
+	JAUNE("jaune", "jaune", "yellow", ChatColor.YELLOW, DyeColor.YELLOW),
+	BLANC("blanc", "blanche", "white", ChatColor.WHITE, DyeColor.WHITE),
+	NOIR("noir", "noire", "black", ChatColor.BLACK, DyeColor.BLACK),
+	ORANGE("orange", "orange", "orange", ChatColor.GOLD, DyeColor.ORANGE),
+	GRIS("gris", "grise", "gray", ChatColor.GRAY, DyeColor.GRAY),
+	NO_COLOR("no color", "no color", "no color", ChatColor.WHITE, DyeColor.WHITE);
 
 	private String maleColor;
 	private String femColor;
 	private String enColor;
 	private ChatColor chatColor;
-	private byte data;
+	private DyeColor dyeColor;
 	
 	public static final int GENRE_F = 0;
 	public static final int GENRE_M = 1;
 
-	private Color(String maleColor, String femColor, String enColor, ChatColor chatColor, byte data)
+	private Color(String maleColor, String femColor, String enColor, ChatColor chatColor, DyeColor dyeColor)
 	{
 		this.maleColor = maleColor;
 		this.femColor = femColor;
 		this.enColor = enColor;
 		this.chatColor = chatColor;
-		this.data = data;
+		this.dyeColor = dyeColor;
 	}
 
 	public static Color forName(String name)
@@ -69,8 +70,8 @@ public enum Color
 		return chatColor;
 	}
 
-	public byte getData()
+	public DyeColor getDyeColor()
 	{
-		return data;
+		return dyeColor;
 	}
 }
