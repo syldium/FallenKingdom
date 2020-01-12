798132864b4c6b69de21724cedb1abfc40c9d4fb
Faymoon <mateo.mercier@laposte.net>
2020-01-12 18:15:22 +0100

    Update Color.java

--- src/fr/devsylone/fkpi/util/Color.java
+++ src/fr/devsylone/fkpi/util/Color.java
@@ -1,42 +1,45 @@
 package fr.devsylone.fkpi.util;
 
 import org.bukkit.ChatColor;
-import org.bukkit.DyeColor;
 
 public enum Color
 {
-	BLEU("bleu", "bleue", ChatColor.BLUE, DyeColor.BLUE),
-	ROUGE("rouge", "rouge", ChatColor.RED, DyeColor.RED),
-	VERT("vert", "verte", ChatColor.GREEN, DyeColor.GREEN),
-	VIOLET("violet", "violette", ChatColor.DARK_PURPLE, DyeColor.PURPLE),
-	ROSE("rose", "rose", ChatColor.LIGHT_PURPLE, DyeColor.MAGENTA),
-	JAUNE("jaune", "jaune", ChatColor.YELLOW, DyeColor.YELLOW),
-	BLANC("blanc", "blanche", ChatColor.WHITE, DyeColor.WHITE),
-	NOIR("noir", "noire", ChatColor.BLACK, DyeColor.BLACK),
-	ORANGE("orange", "orange", ChatColor.GOLD, DyeColor.ORANGE),
-	GRIS("gris", "grise", ChatColor.GRAY, DyeColor.GRAY),
-	NO_COLOR("no color", "no color", ChatColor.WHITE, DyeColor.WHITE);
+	
+
+	BLEU("bleu", "bleue", "blue", ChatColor.BLUE, (byte) 11),
+	ROUGE("rouge", "rouge", "red", ChatColor.RED, (byte) 14),
+	VERT("vert", "verte", "green", ChatColor.GREEN, (byte) 5),
+	VIOLET("violet", "violette", "purple", ChatColor.DARK_PURPLE, (byte) 10),
+	ROSE("rose", "rose", "rose", ChatColor.LIGHT_PURPLE, (byte) 6),
+	JAUNE("jaune", "jaune", "yellow", ChatColor.YELLOW, (byte) 4),
+	BLANC("blanc", "blanche", "white", ChatColor.WHITE, (byte) 0),
+	NOIR("noir", "noire", "black", ChatColor.BLACK, (byte) 15),
+	ORANGE("orange", "orange", "orange", ChatColor.GOLD, (byte) 1),
+	GRIS("gris", "grise", "gray", ChatColor.GRAY, (byte) 8),
+	NO_COLOR("no color", "no color", "no color", ChatColor.WHITE, (byte) 0);
 
 	private String maleColor;
 	private String femColor;
+	private String enColor;
 	private ChatColor chatColor;
-	private DyeColor dyeColor;
+	private byte data;
 	
 	public static final int GENRE_F = 0;
 	public static final int GENRE_M = 1;
 
-	Color(String maleColor, String femColor, ChatColor chatColor, DyeColor dyeColor)
+	private Color(String maleColor, String femColor, String enColor, ChatColor chatColor, byte data)
 	{
 		this.maleColor = maleColor;
 		this.femColor = femColor;
+		this.enColor = enColor;
 		this.chatColor = chatColor;
-		this.dyeColor = dyeColor;
+		this.data = data;
 	}
 
 	public static Color forName(String name)
 	{
 		for(Color c : Color.values())
-			if(name.equalsIgnoreCase(c.maleColor) || name.equalsIgnoreCase(c.femColor))
+			if(name.equalsIgnoreCase(c.maleColor) || name.equalsIgnoreCase(c.femColor) || name.equalsIgnoreCase(c.enColor))
 				return c;
 
 		return null;
@@ -55,7 +58,7 @@ public enum Color
 	public static ChatColor getChatColor(String color)
 	{
 		for(Color c : Color.values())
-			if(color.equalsIgnoreCase(c.maleColor) || color.equalsIgnoreCase(c.femColor))
+			if(color.equalsIgnoreCase(c.maleColor) || color.equalsIgnoreCase(c.femColor) || name.equalsIgnoreCase(c.enColor))
 				return c.chatColor;
 
 		return ChatColor.WHITE;
@@ -66,8 +69,8 @@ public enum Color
 		return chatColor;
 	}
 
-	public DyeColor getDyeColor()
+	public byte getData()
 	{
-		return dyeColor;
+		return data;
 	}
 }
