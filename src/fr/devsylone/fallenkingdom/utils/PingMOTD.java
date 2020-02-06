83baa79c5173b2d5673b199f76337145d6038883
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-02-06 23:05:18 +0100

    Display server version and game status

--- src/fr/devsylone/fallenkingdom/utils/PingMOTD.java
+++ src/fr/devsylone/fallenkingdom/utils/PingMOTD.java
@@ -1,25 +1,38 @@
 package fr.devsylone.fallenkingdom.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
 import fr.devsylone.fallenkingdom.Fk;
-import fr.devsylone.fallenkingdom.game.Game.GameState;
 
 
 public class PingMOTD implements Listener {
 	
 	@EventHandler
     public void countDown(final ServerListPingEvent event){
-		if(Fk.getInstance().getGame().getState() == GameState.BEFORE_STARTING) {
-		event.setMotd("§aLe Fallen King n'a pas encore commencé !\n§6Versions : 1.8 - 1.14.4");
-		}
-		else if(Fk.getInstance().getGame().getState() == GameState.STARTED) {
-		event.setMotd("§6La partie est en cours. \n§6Versions : 1.8 - 1.14.4");
-		}
-		else if(Fk.getInstance().getGame().getState() == GameState.PAUSE) {
-		event.setMotd("§cLa partie est en pause. \n§6Versions : 1.8 - 1.14.4");
+		String VERSION = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+		VERSION = VERSION.replace("v", "");
+		VERSION = VERSION.replace("_", ".");
+		//VERSION = VERSION.replace("R", "");
+		VERSION = VERSION.split(".R")[0];
+		
+		switch(Fk.getInstance().getGame().getState()) {
+		  case BEFORE_STARTING:
+			  event.setMotd("§aLa partie n'a pas encore commencé. \n§7" + "Version" + " §8>> §7" + VERSION);
+		    break;
+		  case STARTING:
+			  event.setMotd("§6La partie va très bientôt commencer. \n§7" + "Version" + " §8>> §7" + VERSION);
+		    break;
+		  case STARTED:
+			  event.setMotd("§b§oLa partie a déjà commencé. \n§7" + "Version" + " §8>> §7" + VERSION);
+			    break;
+		  case PAUSE:
+			  event.setMotd("§c§oLa partie est en pause. \n§7" + "Version" + " §8>> §7" + VERSION);
+			    break;
+		  default:
+			  event.setMotd("§7§oChargement du plugin Fallen Kingdom.");
 		}
 	}
 }
