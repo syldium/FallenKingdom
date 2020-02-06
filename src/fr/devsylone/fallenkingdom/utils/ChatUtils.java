73ee300c7b7fd5ebbbcb842424702075d29033a3
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-02-06 23:03:56 +0100

    Estesthetics

--- src/fr/devsylone/fallenkingdom/utils/ChatUtils.java
+++ src/fr/devsylone/fallenkingdom/utils/ChatUtils.java
@@ -2,7 +2,7 @@ package fr.devsylone.fallenkingdom.utils;
 
 public class ChatUtils
 {
-	public static final String DEVSYLONE = "§7§oDEVSYLONE";
+	public static final String INFO = "§7§oDEVSYLONE";
 	public static final String PREFIX = "§c[§6Fallen Kingdom§c] ";
 	public static final String TEAM = "§b[§6Team§b] ";
 	public static final String RULES = "§b[§6Règles§b] ";
