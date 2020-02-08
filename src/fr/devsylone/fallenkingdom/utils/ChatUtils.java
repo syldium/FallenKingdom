e91f501a221c93dc977d6a815ab945a71482f006
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-02-08 22:52:25 +0100

    Reset

--- src/fr/devsylone/fallenkingdom/utils/ChatUtils.java
+++ src/fr/devsylone/fallenkingdom/utils/ChatUtils.java
@@ -2,14 +2,14 @@ package fr.devsylone.fallenkingdom.utils;
 
 public class ChatUtils
 {
-	public static final String INFO = "§7§oDEVSYLONE";
-	public static final String PREFIX = "§c[§6Fallen Kingdom§c] ";
-	public static final String TEAM = "§b[§6Team§b] ";
-	public static final String RULES = "§b[§6Règles§b] ";
-	public static final String GAME = "§b[§6Jeux§b] ";
-	public static final String CHESTS = "§b[§6Coffres§b] ";
-	public static final String SCOREBOARD = "§b[§6ScoreBoard§b] ";
-	public static final String ALERT = "§b§l[ALERTE§b] ";
+	public static final String DEVSYLONE = "§1§odevsylone";
+	public static final String PREFIX = "§7[§5Fk§7] ";
+	public static final String TEAM = "§7[§bTeams§7] ";
+	public static final String RULES = "§7[§bRules§7] ";
+	public static final String GAME = "§7[§bGame§7] ";
+	public static final String CHESTS = "§7[§bChests§7] ";
+	public static final String SCOREBOARD = "§7[§bScoreboard§7] ";
+	public static final String ALERT = "§4§l[§c§lAlert§4§l] ";
 	public static final String DEBUG = "§7[§cDebug§7] ";
-	public static final String TIP = "§a[§6Tip§b] ";
+	public static final String TIP = "§r[§2Tip§r] ";
 }
