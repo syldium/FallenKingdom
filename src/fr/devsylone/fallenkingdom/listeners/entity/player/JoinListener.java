f20e0b44a06b39feeb343155c122015f723dfcde
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-02-08 22:41:37 +0100

    Conventions

--- src/fr/devsylone/fallenkingdom/listeners/entity/player/JoinListener.java
+++ src/fr/devsylone/fallenkingdom/listeners/entity/player/JoinListener.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Random;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
@@ -45,7 +44,6 @@ public class JoinListener implements Listener
 	public void join(final PlayerJoinEvent e)
 	{
 		FkPlayer player = Fk.getInstance().getPlayerManager().getPlayer(e.getPlayer());
-		Player player_bukkit = e.getPlayer();
 		
 		if(e.getPlayer().isOp())
 			for(String s : Fk.getInstance().getOnConnectWarnings())
@@ -67,26 +65,8 @@ public class JoinListener implements Listener
 		if(player.getState() == PlayerState.EDITING_SCOREBOARD)
 			player.getSbDisplayer().display();
 
-		if(Fk.getInstance().getGame().getState() == GameState.BEFORE_STARTING || Fk.getInstance().getGame().getState() == GameState.PAUSE) {
-			/*
-			List<Integer> EFFECTS = new ArrayList<Integer>();
-			EFFECTS.add(4);
-			EFFECTS.add(6);
-			EFFECTS.add(9);
-			EFFECTS.add(11);
-			EFFECTS.add(17);
-			EFFECTS.add(26);
-			EFFECTS.add(28);
-			EFFECTS.add(32);
-			
-			//https://github.com/kvq/Trails/wiki/API
-			 int RANDOM_EFFECT = getRandomElement(EFFECTS);
-			 //p.sendMessage(Integer.toString(RANDOM_EFFECT));
-			 SuperTrailsAPI.setTrail(RANDOM_EFFECT, player_bukkit);
-			 */
-			 FastParticle.spawnParticle(player_bukkit, ParticleType.CLOUD, player_bukkit.getLocation(), 1000);
-			}
-		
+		if(Fk.getInstance().getGame().getState() == GameState.BEFORE_STARTING || Fk.getInstance().getGame().getState() == GameState.PAUSE)
+			 FastParticle.spawnParticle(e.getPlayer(), ParticleType.CLOUD, e.getPlayer().getLocation(), 1000);
 		}
 	
 
