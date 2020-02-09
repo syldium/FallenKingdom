6246fae48c208774f3f5529d66df5c47bd4242db
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-02-09 23:55:58 +0100

    Several additions (Cancel damages, fireworks, lightning)
    - Cancel damage in PAUSE, BEFORE_STARTING, STARTING
    -  Fireworks when the player dies in 1.9+.
    - Lightning when the player dies in 1.8

--- src/fr/devsylone/fallenkingdom/listeners/entity/DamageListener.java
+++ src/fr/devsylone/fallenkingdom/listeners/entity/DamageListener.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -20,97 +21,113 @@ import fr.devsylone.fallenkingdom.players.FkPlayer;
 import fr.devsylone.fallenkingdom.players.FkPlayer.PlayerState;
 import fr.devsylone.fallenkingdom.scoreboard.PlaceHolder;
 import fr.devsylone.fallenkingdom.utils.ChatUtils;
+import fr.devsylone.fallenkingdom.utils.FireworksUtils;
 import fr.devsylone.fallenkingdom.utils.FkSound;
 import fr.devsylone.fkpi.rules.ChargedCreepers;
 
-public class DamageListener implements Listener
+public class DamageListener implements Listener 
 {
-	@EventHandler
-	public void damage(EntityDamageEvent e)
-	{
-		if(Fk.getInstance().getGame().getState().equals(GameState.PAUSE))
-			e.setCancelled(true);
-	}
+    
+    @EventHandler
+    public void damage(EntityDamageEvent e) {
+        if (Fk.getInstance().getGame().getState().equals(GameState.PAUSE) || Fk.getInstance().getGame().getState().equals(GameState.BEFORE_STARTING) || Fk.getInstance().getGame().getState().equals(GameState.STARTING))
+            e.setCancelled(true);
+    }
 
-	@EventHandler
-	public void creeperDeath(EntityDeathEvent e)
-	{
-		ChargedCreepers rule = (ChargedCreepers) Fk.getInstance().getFkPI().getRulesManager().getRuleByName("ChargedCreepers");
-		if(e.getEntity() instanceof Creeper && ((Creeper) e.getEntity()).isPowered() && (new Random().nextInt(100) <= rule.getDrop()))
-			e.getDrops().add(new ItemStack(Material.TNT, rule.getTntAmount()));
-	}
+    @EventHandler
+    public void creeperDeath(EntityDeathEvent e) {
+        ChargedCreepers rule = (ChargedCreepers) Fk.getInstance().getFkPI().getRulesManager().getRuleByName("ChargedCreepers");
+        if (e.getEntity() instanceof Creeper && ((Creeper) e.getEntity()).isPowered() && (new Random().nextInt(100) <= rule.getDrop()))
+            e.getDrops().add(new ItemStack(Material.TNT, rule.getTntAmount()));
+    }
 
-	@EventHandler
-	public void dead(PlayerDeathEvent e)
-	{
-		e.setDeathMessage(ChatUtils.PREFIX + e.getDeathMessage());
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if (e.getDamager() instanceof org.bukkit.entity.Firework)
+            e.setCancelled(true);
+    }
 
-		/*
-		 * Si le killer a une team
-		 */
-		if(e.getEntity().getKiller() != null && Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getKiller().getName()) != null)
-		{
-			/*
-			 * Sound
-			 */
-			for(Player p : Bukkit.getOnlinePlayers())
-				p.playSound(p.getLocation(), FkSound.WITHER_SPAWN.bukkitSound(), 1, 1);
+    @EventHandler
+    public void dead(PlayerDeathEvent e) {
+        e.setDeathMessage(ChatUtils.PREFIX + e.getDeathMessage());
 
-			/*
-			 * Replace color killer
-			 */
-			e.setDeathMessage(e.getDeathMessage().replaceAll(e.getEntity().getKiller().getName(), e.getEntity().getKiller().getDisplayName()));
+        /*
+         ***INSTANT FIREWORK 1.9+ | ECLAIR 1.8***
+         */
+        if (e.getEntity().getKiller() instanceof Player && e.getEntity() instanceof Player) {
+            if (Bukkit.getServer().getClass().getPackage().getName().contains("1_8")) {
+                e.getEntity().getWorld().strikeLightningEffect(e.getEntity().getLocation());
+            } else {
+                for (int i = 0; i < 5; i++) {
+                    FireworksUtils.random(e.getEntity().getLocation());
+                }
+            }
 
-			/*
-			 * Add kill Si killer != dead
-			*/
-			if(e.getEntity().getKiller() != null && e.getEntity().getKiller().getName() != e.getEntity().getName())
-				Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getKiller().getName()).addKill();
-		}
-		/*
-		 * Replace color killed
-		 */
-		if(Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getName()) != null)
-			e.setDeathMessage(e.getDeathMessage().replaceAll(e.getEntity().getName(), e.getEntity().getDisplayName()));
+            /*
+             * Si le killer a une team
+             */
+            if (e.getEntity().getKiller() != null && Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getKiller().getName()) != null) {
+                /*
+                 * Sound
+                 */
+                for (Player p: Bukkit.getOnlinePlayers())
+                    p.playSound(p.getLocation(), FkSound.WITHER_SPAWN.bukkitSound(), 1, 1);
 
-		/*
-		 * Si tué ou tueur pas de team pas de deathlimit
-		 */
-		if(Fk.getInstance().getGame().getState().equals(GameState.BEFORE_STARTING) || Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getName()) == null || (e.getEntity().getKiller() != null && Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getKiller().getName()) == null))
-			return;
+                /*
+                 * Replace color killer
+                 */
+                e.setDeathMessage(e.getDeathMessage().replaceAll(e.getEntity().getKiller().getName(), e.getEntity().getKiller().getDisplayName()));
 
-		/*
-		 * Add death
-		 */
-		Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName()).addDeath();
+                /*
+                 * Add kill Si killer != dead
+                 */
+                if (e.getEntity().getKiller() != null && e.getEntity().getKiller().getName() != e.getEntity().getName())
+                    Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getKiller().getName()).addKill();
+            }
+            /*
+             * Replace color killed
+             */
+            if (Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getName()) != null)
+                e.setDeathMessage(e.getDeathMessage().replaceAll(e.getEntity().getName(), e.getEntity().getDisplayName()));
 
-		/*
-		 * DeathLimit > 0
-		 */
-		if((int) Fk.getInstance().getFkPI().getRulesManager().getRuleByName("DeathLimit").getValue() > 0)
-			if(Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName()).getDeaths() >= (int) Fk.getInstance().getFkPI().getRulesManager().getRuleByName("DeathLimit").getValue())
-			{
-				/*
-				 * Elimination
-				 */
+            /*
+             * Si tué ou tueur pas de team pas de deathlimit
+             */
+            if (Fk.getInstance().getGame().getState().equals(GameState.BEFORE_STARTING) || Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getName()) == null || (e.getEntity().getKiller() != null && Fk.getInstance().getFkPI().getTeamManager().getPlayerTeam(e.getEntity().getKiller().getName()) == null))
+                return;
 
-				Fk.broadcast(e.getEntity().getDisplayName() + " §4est éliminé !");
-				e.getEntity().setGameMode(GameMode.SPECTATOR);
+            /*
+             * Add death
+             */
+            Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName()).addDeath();
 
-				for(Player p : Bukkit.getOnlinePlayers())
-					p.playSound(p.getLocation(), FkSound.ENDERDRAGON_DEATH.bukkitSound(), 1, 1);
-			}
-			/*
-			 * Info nbre de vie
-			 */
-			else
-				e.getEntity().sendMessage("§cVous avez déjà perdu " + Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName()).getDeaths() + "/" + Fk.getInstance().getFkPI().getRulesManager().getRuleByName("DeathLimit").getValue() + " vie(s)");
+            /*
+             * DeathLimit > 0
+             */
+            if ((int) Fk.getInstance().getFkPI().getRulesManager().getRuleByName("DeathLimit").getValue() > 0)
+                if (Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName()).getDeaths() >= (int) Fk.getInstance().getFkPI().getRulesManager().getRuleByName("DeathLimit").getValue()) {
+                    /*
+                     * Elimination
+                     */
 
-		FkPlayer fkP = Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName());
-		if(fkP.getState() == PlayerState.EDITING_SCOREBOARD)
-			fkP.getSbDisplayer().exit();
+                    Fk.broadcast(e.getEntity().getDisplayName() + " §4est éliminé !");
+                    e.getEntity().setGameMode(GameMode.SPECTATOR);
 
-		Fk.getInstance().getScoreboardManager().refreshAllScoreboards(PlaceHolder.DEATHS);
-		Fk.getInstance().getScoreboardManager().refreshAllScoreboards(PlaceHolder.KILLS);
-	}
+                    for (Player p: Bukkit.getOnlinePlayers())
+                        p.playSound(p.getLocation(), FkSound.ENDERDRAGON_DEATH.bukkitSound(), 1, 1);
+                }
+            /*
+             * Info nbre de vie
+             */
+            else
+                e.getEntity().sendMessage("§cVous avez déjà perdu " + Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName()).getDeaths() + "/" + Fk.getInstance().getFkPI().getRulesManager().getRuleByName("DeathLimit").getValue() + " vie(s)");
+
+            FkPlayer fkP = Fk.getInstance().getPlayerManager().getPlayer(e.getEntity().getName());
+            if (fkP.getState() == PlayerState.EDITING_SCOREBOARD)
+                fkP.getSbDisplayer().exit();
+
+            Fk.getInstance().getScoreboardManager().refreshAllScoreboards(PlaceHolder.DEATHS);
+            Fk.getInstance().getScoreboardManager().refreshAllScoreboards(PlaceHolder.KILLS);
+        }
+    }
 }
