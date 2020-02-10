252fb99b037efd443c945cb99be12eaba53d135f
Ottawas <40337934+Ottawas@users.noreply.github.com>
2020-02-10 21:37:44 +0100

    Addition of a condition for changing the MOTD

--- src/fr/devsylone/fallenkingdom/listeners/motd/MotdListener.java
+++ src/fr/devsylone/fallenkingdom/listeners/motd/MotdListener.java
@@ -13,23 +13,25 @@ public class MotdListener implements Listener {
     @EventHandler
     public void countDown(final ServerListPingEvent event) {
 
-        String serverVersion = (((Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3]).replace("v", "")).replace("_", ".")).split(".R")[0];;
+        if (event.getMotd().equalsIgnoreCase("A Minecraft Server")) {
+            String serverVersion = (((Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3]).replace("v", "")).replace("_", ".")).split(".R")[0];;
 
-        switch (Fk.getInstance().getGame().getState()) {
-            case BEFORE_STARTING:
-                event.setMotd("§aLa partie n'a pas encore commencé. \n§7" + "Version" + " §8>> §7" + serverVersion);
-                break;
-            case STARTING:
-                event.setMotd("§6La partie va très bientôt commencer. \n§7" + "Version" + " §8>> §7" + serverVersion);
-                break;
-            case STARTED:
-                event.setMotd("§bLa partie a déjà commencé. \n§7" + "Version" + " §8>> §7" + serverVersion);
-                break;
-            case PAUSE:
-                event.setMotd("§cLa partie est en pause. \n§7" + "Version" + " §8>> §7" + serverVersion);
-                break;
-            default:
-                event.setMotd("§7§oChargement du plugin Fallen Kingdom.");
+            switch (Fk.getInstance().getGame().getState()) {
+                case BEFORE_STARTING:
+                    event.setMotd("§aLa partie n'a pas encore commencé. \n§7" + "Version" + " §8>> §7" + serverVersion);
+                    break;
+                case STARTING:
+                    event.setMotd("§6La partie va très bientôt commencer. \n§7" + "Version" + " §8>> §7" + serverVersion);
+                    break;
+                case STARTED:
+                    event.setMotd("§bLa partie a déjà commencé. \n§7" + "Version" + " §8>> §7" + serverVersion);
+                    break;
+                case PAUSE:
+                    event.setMotd("§cLa partie est en pause. \n§7" + "Version" + " §8>> §7" + serverVersion);
+                    break;
+                default:
+                    event.setMotd("§7§oChargement du plugin FallenKingdom.");
+            }
         }
     }
 }
