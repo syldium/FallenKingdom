647099226bbd835c870a6b2ff03fe592927f4eb2
Etrenak <etrenak@gmail.com>
2020-05-19 10:04:42 +0200

    Update fkUpdater link

--- src/fr/devsylone/fallenkingdom/updater/PluginUpdater.java
+++ src/fr/devsylone/fallenkingdom/updater/PluginUpdater.java
@@ -39,7 +39,6 @@ public class PluginUpdater extends BukkitRunnable
     private boolean enabled = true;
     private static final Gson GSON;
     private static final String URL_RELEASE_LIST = "https://servermods.forgesvc.net/servermods/files?projectIds=276763";
-    private static final String URL_UPDATER = "http://fkdevsylone.000webhostapp.com/FK/auto-update/version/updater";
     static
     {
         GSON = new GsonBuilder().registerTypeAdapter(Instant.class, new JsonDeserializer<Instant>()
@@ -124,7 +123,13 @@ public class PluginUpdater extends BukkitRunnable
                     this.plugin.getLogger().info("[Updater] Version téléchargée");
                     this.plugin.getLogger().info("[Updater] Téléchargement de l'updater...");
 
-                    rbc = Channels.newChannel(new URL(URL_UPDATER).openStream());
+                    ReleaseInfo latestUpdaterRelease = Streams.findLast(Arrays.stream(releases).filter(info -> info.getReleaseType().isRelease() && info.getName().startsWith("FkUpdater"))).orElse(null);
+                    if(latestUpdaterRelease == null)
+                    {
+                        this.plugin.getLogger().warning("[Updater] Une erreur est survenue : Impossible de trouver le plugin de mise à jour");
+                        return;
+                    }
+                    rbc = Channels.newChannel(latestUpdaterRelease.getDownloadUrl().openStream());
                     final File updater = new File(Fk.getInstance().getDataFolder().getParent() + "/FkUpdater.jar");
                     fos = new FileOutputStream(updater);
                     fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
