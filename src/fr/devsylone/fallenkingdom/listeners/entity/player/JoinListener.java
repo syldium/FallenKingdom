2b5d4a580a797609482fe0630ddc7fadda9c6481
syldium <syldium@mailo.com>
2020-11-07 13:32:23 +0100

    Fix test env

--- src/fr/devsylone/fallenkingdom/listeners/entity/player/JoinListener.java
+++ src/fr/devsylone/fallenkingdom/listeners/entity/player/JoinListener.java
@@ -3,11 +3,14 @@ package fr.devsylone.fallenkingdom.listeners.entity.player;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
+import java.util.List;
+
 import fr.devsylone.fallenkingdom.Fk;
 import fr.devsylone.fallenkingdom.players.FkPlayer;
 import fr.devsylone.fallenkingdom.players.FkPlayer.PlayerState;
 import fr.devsylone.fallenkingdom.game.Game.GameState;
 import fr.devsylone.fallenkingdom.utils.Messages;
+import fr.devsylone.fallenkingdom.utils.NMSUtils;
 import fr.devsylone.fkpi.teams.Team;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,12 +20,13 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
-import org.bukkit.inventory.PlayerInventory;
 import com.cryptomorin.xseries.SkullUtils;
 import com.cryptomorin.xseries.XMaterial;
 
 public class JoinListener implements Listener
 {
+	private final boolean NON_TESTING_ENV = NMSUtils.obcOptionalClass("inventory.CraftMetaSkull").isPresent();
+
 	@EventHandler
 	public void prelogin(final AsyncPlayerPreLoginEvent e)
 	{
@@ -59,11 +63,13 @@ public class JoinListener implements Listener
 
 		if(player.getState() == PlayerState.EDITING_SCOREBOARD)
 			player.getSbDisplayer().display();
-			
-		LocalDate currentDate = LocalDate.now();
 
-		if(Fk.getInstance().getGame().getState().equals(GameState.BEFORE_STARTING) && e.getPlayer().getInventory().getHelmet() == null && (currentDate.getDayOfMonth() == 12) && (currentDate.getMonth() == Month.JUNE))
-			e.getPlayer().getInventory().setHelmet(head());
+		if(NON_TESTING_ENV)
+		{
+			LocalDate currentDate = LocalDate.now();
+			if(Fk.getInstance().getGame().getState().equals(GameState.BEFORE_STARTING) && e.getPlayer().getInventory().getHelmet() == null && (currentDate.getDayOfMonth() == 12) && (currentDate.getMonth() == Month.JUNE))
+				e.getPlayer().getInventory().setHelmet(head(currentDate));
+		}
 	}
 
 	@EventHandler
@@ -87,17 +93,15 @@ public class JoinListener implements Listener
 				+ "§7Erreur : §c" + Fk.getInstance().getPluginError();
 	}
 	
-	private ItemStack head()
+	private ItemStack head(LocalDate date)
 	{
-		LocalDate currentDate = LocalDate.now();
-
 		ItemStack skull = new ItemStack(XMaterial.PLAYER_HEAD.parseItem());
 		SkullMeta skullMeta = SkullUtils.applySkin(skull.getItemMeta(), "ewogICJ0aW1lc3RhbXAiIDogMTYwNDUxNzcwODc5OSwKICAicHJvZmlsZUlkIiA6ICJhZmI0ODljNDlmYzg0OGE0OThmMmRkN2JlYTQxNGM5YSIsCiAgInByb2ZpbGVOYW1lIiA6ICJNSEZfQ2FrZSIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS9lYzI0MWE1OTdjMjg1ZTEwNGMyNzExOTZkNzg1ZGI0Y2QwMTEwYTQwYzhmOGU1ZDM1NGM1NjQ0MTU5NTY3YzlkIgogICAgfQogIH0KfQ=="); // MHF_Cake's skin
 		skullMeta.setDisplayName(Messages.EASTER_EGG_ANNIVERSARY_NAME.getMessage());
-		ArrayList<String> lore = new ArrayList<String>();
+		List<String> lore = new ArrayList<>();
 		lore.add(Messages.EASTER_EGG_ANNIVERSARY_LORE_1.getMessage());
 		lore.add(Messages.EASTER_EGG_ANNIVERSARY_LORE_2.getMessage());
-		lore.add(Messages.EASTER_EGG_ANNIVERSARY_LORE_3.getMessage().replace("%age%", Integer.toString(currentDate.getYear() - 2016)));
+		lore.add(Messages.EASTER_EGG_ANNIVERSARY_LORE_3.getMessage().replace("%age%", Integer.toString(date.getYear() - 2016)));
 		skullMeta.setLore(lore);
 		skull.setItemMeta(skullMeta);
 		return skull;
