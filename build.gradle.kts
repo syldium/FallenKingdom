57ae6a33c0863b8abfe4e08561e4a3eaca94f648
syldium <syldium@mailo.com>
2021-04-23 20:58:42 +0200

    Initial commit - Setup Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.gradle.api.plugins.JavaPlugin.*
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "6.1.0" apply false
+    idea
+}
+
+allprojects {
+    group = "fr.devsylone"
+    version = "3.0.0-SNAPSHOT"
+    description = "A minecraft gamemode plugin"
+}
+
+plugins.apply("idea")
+
+subprojects {
+    repositories {
+        mavenCentral()
+
+        // <editor-fold desc="Repositories" defaultstate="collapsed">
+        // {{{ Repositories
+        maven {
+            name = "papermc"
+            url = uri("https://papermc.io/repo/repository/maven-public/")
+
+            content {
+                includeGroup("com.destroystokyo.paper")
+                includeGroup("io.papermc")
+            }
+        }
+        // }}}
+        // </editor-fold>
+
+        maven("https://repo.codemc.org/repository/maven-public")
+        maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    }
+
+    dependencies {
+        COMPILE_ONLY_API_CONFIGURATION_NAME("org.checkerframework", "checker-qual",  "3.9.1")
+        TEST_IMPLEMENTATION_CONFIGURATION_NAME("org.junit.jupiter", "junit-jupiter-engine", "5.7.0")
+        COMPILE_ONLY_API_CONFIGURATION_NAME("com.google.errorprone", "error_prone_annotations", "2.5.1")
+        COMPILE_ONLY_API_CONFIGURATION_NAME("org.jetbrains", "annotations", "20.1.0")
+    }
+}
