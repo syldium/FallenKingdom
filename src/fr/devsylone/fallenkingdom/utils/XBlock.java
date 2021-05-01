e5207f37c616a263e1284e517f0c38296ad1fd33
syldium <syldium@mailo.com>
2021-05-01 16:17:25 +0200

    Use Material#valueOf instead of Material#getMaterial

--- src/fr/devsylone/fallenkingdom/utils/XBlock.java
+++ src/fr/devsylone/fallenkingdom/utils/XBlock.java
@@ -102,18 +102,18 @@ public final class XBlock {
     public static boolean setColor(Block block, DyeColor color) {
         if (ISFLAT) {
             String type = block.getType().name();
-            if (type.endsWith("WOOL")) block.setType(Material.getMaterial(color.name() + "_WOOL"));
-            else if (type.endsWith("BED")) block.setType(Material.getMaterial(color.name() + "_BED"));
-            else if (type.endsWith("STAINED_GLASS")) block.setType(Material.getMaterial(color.name() + "_STAINED_GLASS"));
-            else if (type.endsWith("STAINED_GLASS_PANE")) block.setType(Material.getMaterial(color.name() + "_STAINED_GLASS_PANE"));
-            else if (type.endsWith("TERRACOTTA")) block.setType(Material.getMaterial(color.name() + "_TERRACOTTA"));
-            else if (type.endsWith("GLAZED_TERRACOTTA")) block.setType(Material.getMaterial(color.name() + "_GLAZED_TERRACOTTA"));
-            else if (type.endsWith("BANNER")) block.setType(Material.getMaterial(color.name() + "_BANNER"));
-            else if (type.endsWith("WALL_BANNER")) block.setType(Material.getMaterial(color.name() + "_WALL_BANNER"));
-            else if (type.endsWith("CARPET")) block.setType(Material.getMaterial(color.name() + "_CARPET"));
-            else if (type.endsWith("SHULKER_BOX")) block.setType(Material.getMaterial(color.name() + "_SHULKERBOX"));
-            else if (type.endsWith("CONCRETE")) block.setType(Material.getMaterial(color.name() + "_CONCRETE"));
-            else if (type.endsWith("CONCRETE_POWDER")) block.setType(Material.getMaterial(color.name() + "_CONCRETE_POWDER"));
+            if (type.endsWith("WOOL")) block.setType(Material.valueOf(color.name() + "_WOOL"));
+            else if (type.endsWith("BED")) block.setType(Material.valueOf(color.name() + "_BED"));
+            else if (type.endsWith("STAINED_GLASS")) block.setType(Material.valueOf(color.name() + "_STAINED_GLASS"));
+            else if (type.endsWith("STAINED_GLASS_PANE")) block.setType(Material.valueOf(color.name() + "_STAINED_GLASS_PANE"));
+            else if (type.endsWith("TERRACOTTA")) block.setType(Material.valueOf(color.name() + "_TERRACOTTA"));
+            else if (type.endsWith("GLAZED_TERRACOTTA")) block.setType(Material.valueOf(color.name() + "_GLAZED_TERRACOTTA"));
+            else if (type.endsWith("BANNER")) block.setType(Material.valueOf(color.name() + "_BANNER"));
+            else if (type.endsWith("WALL_BANNER")) block.setType(Material.valueOf(color.name() + "_WALL_BANNER"));
+            else if (type.endsWith("CARPET")) block.setType(Material.valueOf(color.name() + "_CARPET"));
+            else if (type.endsWith("SHULKER_BOX")) block.setType(Material.valueOf(color.name() + "_SHULKERBOX"));
+            else if (type.endsWith("CONCRETE")) block.setType(Material.valueOf(color.name() + "_CONCRETE"));
+            else if (type.endsWith("CONCRETE_POWDER")) block.setType(Material.valueOf(color.name() + "_CONCRETE_POWDER"));
             else return false;
             return true;
         }
