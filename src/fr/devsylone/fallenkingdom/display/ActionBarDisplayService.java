90449b544dc08fe6f9fd6783004ef47227dbe6e9
syldium <syldium@mailo.com>
2021-11-06 13:57:47 +0100

    Support action bars on 1.8 servers

--- src/fr/devsylone/fallenkingdom/display/ActionBarDisplayService.java
+++ src/fr/devsylone/fallenkingdom/display/ActionBarDisplayService.java
@@ -1,24 +1,37 @@
 package fr.devsylone.fallenkingdom.display;
 
 import fr.devsylone.fallenkingdom.players.FkPlayer;
+import fr.devsylone.fallenkingdom.utils.NMSUtils;
+import fr.devsylone.fallenkingdom.utils.PacketUtils;
+import fr.devsylone.fallenkingdom.version.tracker.ChatMessage;
 import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Constructor;
 import java.util.function.BiConsumer;
 
 public class ActionBarDisplayService extends SimpleDisplayService {
 
-    public static final BiConsumer<Player, String> SEND_ACTION_BAR;
+    /**
+     * Envoie le message dans la barre d'action.
+     */
+    public static final BiConsumer<@NotNull Player, @NotNull String> SEND_ACTION_BAR;
 
     static {
         BiConsumer<Player, String> sendActionBar;
         try {
             Player.class.getMethod("sendActionBar", String.class);
             sendActionBar = Player::sendActionBar;
-        } catch (NoSuchMethodException ex) {
-            sendActionBar = (player, message) -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+        } catch (NoSuchMethodException ignored) {
+            try {
+                Player.Spigot.class.getMethod("sendMessage", ChatMessageType.class, BaseComponent[].class);
+                sendActionBar = (player, message) -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+            } catch (NoSuchMethodException nms) {
+                sendActionBar = new NMSActionBar();
+            }
         }
         SEND_ACTION_BAR = sendActionBar;
     }
@@ -40,5 +53,30 @@ public class ActionBarDisplayService extends SimpleDisplayService {
     @Override
     public @NotNull ActionBarDisplayService withValue(@NotNull String next) {
         return new ActionBarDisplayService(next);
+    }
+
+    // 1.8 :cry:
+    private static class NMSActionBar implements BiConsumer<Player, String> {
+
+        private static final Constructor<?> PACKET_CONSTRUCTOR;
+
+        static {
+            try {
+                final Class<?> packet = NMSUtils.nmsClass("network.protocol.game", "PacketPlayOutChat");
+                PACKET_CONSTRUCTOR = packet.getConstructor(ChatMessage.CHAT_BASE_COMPONENT, byte.class);
+            } catch (ClassNotFoundException | NoSuchMethodException ex) {
+                throw new ExceptionInInitializerError(ex);
+            }
+        }
+
+        @Override
+        public void accept(Player player, String message) {
+            try {
+                PacketUtils.sendPacket(player, PACKET_CONSTRUCTOR.newInstance(ChatMessage.fromString(message), (byte) 2));
+            } catch (ReflectiveOperationException ex) {
+                ex.printStackTrace();
+                player.sendMessage(message);
+            }
+        }
     }
 }
