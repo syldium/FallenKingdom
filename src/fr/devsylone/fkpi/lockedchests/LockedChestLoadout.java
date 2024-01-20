fc7906415ce78bba349b5d1c2e321541427d5071
UnikMask <alexandre.guerville@hotmail.com>
2024-01-20 14:50:19 +0100

    feat(lockedchest): Add advancement and loot table to loadout

--- src/fr/devsylone/fkpi/lockedchests/LockedChestLoadout.java
+++ src/fr/devsylone/fkpi/lockedchests/LockedChestLoadout.java
@@ -1,21 +1,58 @@
 package fr.devsylone.fkpi.lockedchests;
 
+import static fr.devsylone.fallenkingdom.utils.KeyHelper.parseKey;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.loot.LootContext;
+import org.jetbrains.annotations.NotNull;
+import fr.devsylone.fallenkingdom.version.Version.VersionType;
 import fr.devsylone.fkpi.util.Saveable;
 import lombok.Getter;
 
 public class LockedChestLoadout implements Saveable {
     @Getter
+    private int time = -1;
     private List<ItemStack> inventory = new ArrayList<>();
+    private String lootTable;
     @Getter
-    private int time;
+    private String advancement = new String();
+
+    enum ChestKind {
+        SET_INVENTORY, LOOT_TABLE
+    }
+
+    private ChestKind kind;
+
+    /**
+     * Getter for the chest's inventory. If the chest loadout is based off a loot table, returns an
+     * inventory based on the loot table.
+     *
+     * @param {@link Location} of the chest.
+     *
+     * @return List of {@link ItemStack} as inventory.
+     */
+    public List<ItemStack> getInventory(@NotNull Location loc) {
+        if (kind == ChestKind.LOOT_TABLE) {
+            return new ArrayList<>(Bukkit.getLootTable(parseKey(lootTable))
+                    .populateLoot(new Random(), new LootContext.Builder(loc).build()));
+        }
+        return inventory;
+    }
 
 
+    /**
+     * Loads a new loadout object from a configuration.
+     *
+     * @param config {@link ConfigurationSection}
+     * @return {@link LockedChestLoadout}
+     */
     public static LockedChestLoadout from(ConfigurationSection config) {
         LockedChestLoadout loadout = new LockedChestLoadout();
         loadout.load(config);
@@ -23,11 +60,35 @@ public class LockedChestLoadout implements Saveable {
     }
 
 
+    // -- Saveable -- //
+
+
     @Override
     public void load(ConfigurationSection config) {
-        if (!config.contains("time") || !config.isInt("time")) {
-            time = -1;
+        if (config.isInt("time")) {
+            time = config.getInt("time");
         }
+        if (config.isString("Advancement")) {
+            advancement = config.getString("Advancement");
+        }
+        switch (config.getString("kind")) {
+            case "LootTable":
+                kind = ChestKind.LOOT_TABLE;
+                break;
+            default:
+                kind = ChestKind.SET_INVENTORY;
+        }
+
+        // Loot table
+        if (!VersionType.V1_13.isHigherOrEqual()) {
+            kind = ChestKind.SET_INVENTORY;
+        }
+        if (kind == ChestKind.LOOT_TABLE && config.isString("LootTable")) {
+            lootTable = config.getString("LootTable");
+            return;
+        }
+
+        // Inventory
         if (!config.contains("inventory") || !config.isConfigurationSection("inventory")) {
             return;
         }
@@ -41,15 +102,20 @@ public class LockedChestLoadout implements Saveable {
                 inventory.add(invConfig.getItemStack(i.toString()));
             }
         }
-
     }
 
     @Override
     public void save(ConfigurationSection config) {
         config.set("time", time);
-        ConfigurationSection invConfig = config.createSection("inventory");
-        for (Integer i = 0; i < inventory.size(); i++) {
-            invConfig.set(i.toString(), inventory.get(i));
+        config.set("Advancement", advancement);
+
+        if (kind == ChestKind.SET_INVENTORY) {
+            ConfigurationSection invConfig = config.createSection("inventory");
+            for (Integer i = 0; i < inventory.size(); i++) {
+                invConfig.set(i.toString(), inventory.get(i));
+            }
+        } else {
+            config.set("LootTable", lootTable);
         }
     }
 }
