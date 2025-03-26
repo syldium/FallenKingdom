a7bc8617467258c54137e515eb7e1b693322662a
syldium <syldium@mailo.com>
2025-03-26 19:46:21 +0100

    Port early disabled potions listener for 1.21 servers

--- src/fr/devsylone/fallenkingdom/utils/PotionUtils.java
+++ src/fr/devsylone/fallenkingdom/utils/PotionUtils.java
@@ -5,7 +5,8 @@ import org.bukkit.inventory.ItemStack;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+
+import static fr.devsylone.fallenkingdom.utils.PacketUtils.MINECRAFT_SERVER;
 
 public class PotionUtils
 {
@@ -17,6 +18,9 @@ public class PotionUtils
 	// 1.8 TileEntityBrewingStand#o(): void - 1.9+ PotionBrewer#d(ItemStack, ItemStack): ItemStack
 	private static final Method POTION_BREW;
 
+	// 1.21+ MinecraftServer#brewingRecipeRegistry(): BrewingRecipeRegistry
+	private static final Method GET_BREWING_REGISTRY;
+
 	static {
 		try {
 			if (Version.VersionType.V1_9_V1_12.isHigherOrEqual()) {
@@ -24,10 +28,13 @@ public class PotionUtils
 				TILE_BREWING_STAND_SET_ITEM = null;
 				TILE_BREWING_STAND_GET_ITEM = null;
 
-				POTION_BREW = Arrays.stream(NMSUtils.nmsClass("world.item.alchemy", "PotionBrewer").getDeclaredMethods())
-						.filter(m -> m.getReturnType().equals(XItemStack.ITEM_STACK))
-						.filter(m -> Arrays.equals(m.getParameterTypes(), new Class[]{XItemStack.ITEM_STACK, XItemStack.ITEM_STACK}))
-						.findAny().orElseThrow(RuntimeException::new);
+				Class<?> brewingRegistry = NMSUtils.nmsClass("world.item.alchemy", "PotionBrewing", "PotionBrewer");
+				POTION_BREW = NMSUtils.getMethod(brewingRegistry, XItemStack.ITEM_STACK, XItemStack.ITEM_STACK, XItemStack.ITEM_STACK);
+				Method getBrewingRegistry = null;
+				try {
+					getBrewingRegistry = NMSUtils.getMethod(MINECRAFT_SERVER, brewingRegistry); // 1.21+
+				} catch (NoSuchMethodException ignored) {}
+				GET_BREWING_REGISTRY = getBrewingRegistry;
 			} else {
 				Class<?> tileEntityBrewingStand = NMSUtils.nmsClass("world.level.block.entity", "TileEntityBrewingStand");
 				TILE_BREWING_STAND = tileEntityBrewingStand.getConstructor();
@@ -37,6 +44,7 @@ public class PotionUtils
 						.getMethod("getItem", int.class);
 				POTION_BREW = tileEntityBrewingStand.getDeclaredMethod("o");
 				POTION_BREW.setAccessible(true);
+				GET_BREWING_REGISTRY = null;
 			}
 		} catch (ReflectiveOperationException e) {
 			throw new ExceptionInInitializerError(e);
@@ -47,11 +55,12 @@ public class PotionUtils
 	{
 		try {
 			if (TILE_BREWING_STAND == null) {
+				Object registry = GET_BREWING_REGISTRY == null ? null : GET_BREWING_REGISTRY.invoke(PacketUtils.getNMSServer());
 				ItemStack[] result = new ItemStack[potions.length];
 				for (int slot = 0; slot < potions.length; slot++) {
 					Object craftPotion = XItemStack.asCraftItem(potions[slot]);
 					Object craftIngredient = XItemStack.asCraftItem(ingredient);
-					result[slot] = XItemStack.asBukkitItem(POTION_BREW.invoke(null, craftIngredient, craftPotion));
+					result[slot] = XItemStack.asBukkitItem(POTION_BREW.invoke(registry, craftIngredient, craftPotion));
 				}
 				return result;
 			}
