99877642cb4745054163cdb342f3ce5825962b22
syldium <syldium@mailo.com>
2025-06-26 21:35:28 +0200

    Detect when dispensers are used to waterlog

--- src/fr/devsylone/fallenkingdom/listeners/block/BucketListener.java
+++ src/fr/devsylone/fallenkingdom/listeners/block/BucketListener.java
@@ -7,12 +7,15 @@ import fr.devsylone.fallenkingdom.version.Version;
 import fr.devsylone.fkpi.rules.Rule;
 import fr.devsylone.fkpi.teams.Team;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
 import org.bukkit.block.data.Waterlogged;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
 import org.bukkit.event.player.PlayerBucketEvent;
 import org.bukkit.event.player.PlayerBucketFillEvent;
@@ -34,6 +37,27 @@ public class BucketListener implements Listener
     @EventHandler
     public void onFill(PlayerBucketFillEvent event) {
         onBucket(event, Messages.PLAYER_FILL_BUCKET_NEXT);
+    }
+
+    @EventHandler
+    public void onDispense(@NotNull BlockDispenseEvent event) {
+        final Block sourceBlock = event.getBlock();
+        if (!plugin.getWorldManager().isWorldWithBase(sourceBlock.getWorld())) {
+            return;
+        }
+        if (!plugin.getFkPI().getRulesManager().getRule(Rule.BLAST_PROOF_BASE)
+                && Version.VersionType.V1_13.isHigherOrEqual()
+                && event.getItem().getType() == Material.WATER_BUCKET) {
+            final BlockData blockData = sourceBlock.getBlockData();
+            if (blockData instanceof Directional) {
+                final Block targetBlock = sourceBlock.getRelative(((Directional) blockData).getFacing());
+                if (plugin.getFkPI().getTeamManager().getBase(targetBlock).isPresent()) {
+                    if (targetBlock.getBlockData() instanceof Waterlogged) {
+                        event.setCancelled(true);
+                    }
+                }
+            }
+        }
     }
 
     private void onBucket(@NotNull PlayerBucketEvent event, Messages message) {
